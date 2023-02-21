11ab80a866fa018d255fc6acc54f81a0749ba9b1
oscar_shen <cshen5@uw.edu>
2023-02-20 17:37:56 -0800

    AddChart2

--- chart2.R
+++ chart2.R
@@ -1,1 +1,11 @@
 
+library("dplyr")
+#library("stringr")
+library("ggplot2")
+
+mental_data <- read.csv("Mental-health-Depression-disorder-Data.csv", stringsAsFactors = FALSE)
+View(mental_data)
+
+ggplot(mental_data, aes(x = Anxiety.disorders....)) +
+  geom_histogram()
+
