509f07ef3a8645571f0c63059b9a4c5beca0e9c4
oscar_shen <cshen5@uw.edu>
2023-02-20 23:59:33 -0800

    histogram

--- chart2.R
+++ chart2.R
@@ -6,6 +6,12 @@ library("ggplot2")
 mental_data <- read.csv("Mental-health-Depression-disorder-Data.csv", stringsAsFactors = FALSE)
 View(mental_data)
 
-ggplot(mental_data, aes(x = Anxiety.disorders....)) +
-  geom_histogram()
 
+
+ggplot(mental_data, aes(x = Anxiety.disorders...., y = Year)) +
+  geom_histogram() +
+  labs(x = "Year",
+      y = "Number of Anxiety Disorder")
+
+ggplot(mental_data, aes(x = Year)) +
+  geom_histogram()
